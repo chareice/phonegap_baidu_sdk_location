70675c80dd22c17864a6aa5bf34479e24bd2bad1
Chareice <chareice@live.com>
2015-02-04 14:36:16 +0800

    使用高精度定位

--- src/android/BaiduLocation.java
+++ src/android/BaiduLocation.java
@@ -69,7 +69,7 @@ public class BaiduLocation extends CordovaPlugin {
 					option.setOpenGps(true);
 					option.setCoorType("bd09ll");// 返回的定位结果是百度经纬度，默认值gcj02
 					option.setProdName("BaiduLoc");
-					option.setLocationMode(LocationMode.Device_Sensors);
+					option.setLocationMode(LocationMode.Hight_Accuracy);
 					// option.disableCache(true);// 禁止启用缓存定位
 					locationClient.setLocOption(option);
 
